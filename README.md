ce44f63fb35c01adfa8e4bbe50ad7a8d6bf5e994
Stuart Bishop <stuart.bishop@canonical.com>
2019-01-08 19:55:08 +1100

    Refactor into generic k8s charm

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-= Canonical IS Portal =
+= generik8s charm =
 
-== CAAS version ==
+A generic Juju k8s charm for deploying arbitrary container images to k8s services attached to the Juju controller.
 
-Yay documentation.
+== Overview ==
+
+This is a k8s charm and can only be deployed to to a Juju k8s
+pseudo-cloud, attached to a controller using 'juju add-k8s'.
+
+The image to spin up is specified in the 'image' charm configuration
+option using standard docker notation (eg. 'localhost:32000/mywork-rev42').
+Images must be publicly accessible.
+
+Image configuration is specified as YAML snippets in the charm config.
+Both 'container_config' and 'container_secrets' items are provided,
+and they are combined together. 'container_config' gets logged,
+'container_secrets' does not. Variable interpolation is done on the
+configuration, allowing configuration to be pulled from supported
+relations (eg. `${PGHOST}` and similar to pull configuration from a
+PostgreSQL relation; only a single relation is supported).
+
+== Details ==
+
+See config option descriptions in config.yaml.
+
+== Future ==
+
+* Add mechanisms for specifying keys and retrieving images from private repositories, as required.
+* Add mechanism for images to pull secrets from k8s secret stores.
+* Add heathchecks to container spec (confirm something is listening on the opened ports).
--- reactive/container.py
+++ reactive/container.py
@@ -1,25 +1,71 @@
+import io
 from pprint import pprint
+import re
+import string
 import yaml
 
 from charms.layer import caas_base, status
 from charms import reactive
-from charms.reactive import hook, when_not
+from charms.reactive import hook, when, when_any, when_none, tracer
 from charmhelpers.core import hookenv
 
 
 # Run the create_container handler again whenever the config or
 # database relation changes.
 reactive.register_trigger(when='config.changed', clear_flag='container.configured')
-reactive.register_trigger(when='db.master.changed', clear_flag='container.configured')
+reactive.register_trigger(when='postgres.master.changed', clear_flag='container.configured')
+
+reactive.register_trigger(when='config.changed.container_config', clear_flag='container.no-postgres')
+reactive.register_trigger(when='config.changed.container_secrets', clear_flag='container.no-postgres')
 
 
 @hook('upgrade-charm')
 def upgrade_charm():
+    reactive.clear_flag('container.no-postgres')
     reactive.clear_flag('container.configured')
 
 
-@when_not('container.configured')
-# @when('db.master.available') Portal is not using relations to its databases, but it could be.
+@when('config.default.image')
+def block_on_image_config():
+    status.blocked("config item 'image' is required")
+
+
+@when_none('postgres.connected', 'container.no-postgres')
+def block_for_postgres():
+    if postgres_required():
+        hookenv.log("PostgreSQL relation is required")
+        status.blocked('postgres relation is required')
+    else:
+        hookenv.log("PostgreSQL relation is not required")
+        reactive.set_flag('container.no-postgres')
+
+
+@when('postgres.connected')
+@when_none('postgres.master.available', 'container.no-postgres')
+def wait_for_postgres():
+    status.waiting('Waiting for postgres relation to complete')
+
+
+reactive.register_trigger(when='config.changed.pgdatabase', clear_flag='container.postgres.dbset')
+reactive.register_trigger(when='config.changed.pgextensions', clear_flag='container.postgres.dbset')
+reactive.register_trigger(when='config.changed.pgroles', clear_flag='container.postgres.dbset')
+
+
+@when('postgresql.connected')
+@when_none('container.postgres.dbset')
+def setup_postgres():
+    pgsql = reactive.endpoint_from_name('postgres')
+    if pgsql is None:
+        hookenv.log("Expected postgres relation was not found", hookenv.ERROR)
+    config = hookenv.config()
+    pgsql.set_database(config['pgdatabase'])
+    pgsql.set_extensions(set(config['pgextensions'].split(',')))
+    pgsql.set_roles(set(config['pgroles'].split(',')))
+    reactive.set_flag('container.postgres.dbset')
+
+
+@when_none('container.configured', 'config.default.image')
+@when_any('postgres.master.available', 'container.no-postgres')
 def config_container():
     status.maintenance('configuring container')
     spec = make_pod_spec()
@@ -30,14 +76,52 @@ def config_container():
         status.blocked('k8s spec deployment failed. Check logs with kubectl')
 
 
-def make_pod_spec():
+def postgres_required():
+    '''True if container config contains ${PGHOST} style vars'''
+    c = full_container_config()
+    p = re.compile(r'\$\{?PG\w+\}?', re.I)
+    for k, v in c:
+        if p.search(v) is not None:
+            return True
+    return False
+
+
+def sanitized_container_config():
+    '''Uninterpolated container config without secrets'''
+    config = hookenv.config()
+    return yaml.safe_load(config['container_config'])
+
+
+def full_container_config():
+    '''Uninterpolated ontainer config with secrets'''
     config = hookenv.config()
     container_config = yaml.safe_load(config['container_config'])
+    container_config.update(yaml.safe_load(config['container_secrets']))
+    return container_config
 
-    # http://interface-pgsql.readthedocs.io on the PostgreSQL interface
-    # ep = reactive.endpoint_from_flag('db.master.available')
-    # conn_str = ep.master
-    # dbname, host, port = ep.dbname, ep.host, ep.port
+
+def interpolate(container_config):
+    '''Use string.Template to interpolate supported placeholders'''
+    context = {}
+    pgsql = reactive.endpoint_from_name('postgres')
+    if pgsql is not None and pgsql.master is not None:
+        master = pgsql.master
+        context['PGHOST'] = master.host
+        context['PGDATABASE'] = master.dbname
+        context['PGPORT'] = master.port
+        context['PGUSER'] = master.user
+        context['PGPASSWORD'] = master.password
+        context['PGURI'] = master.uri
+    iconfig = {}
+    for k, v in container_config:
+        t = string.Template(v)
+        iconfig[k] = t.safe_substitute(context)
+    return iconfig
+
+
+def make_pod_spec():
+    config = hookenv.config()
+    container_config = interpolate(sanitized_container_config())
 
     ports = [
         {'name': name, 'containerPort': int(port), 'protocol': 'TCP'} for name, port in [
@@ -51,25 +135,15 @@ def make_pod_spec():
                 'image': config['image'],
                 'ports': ports,
                 'config': container_config,
-                # 'files': [
-                #     {
-                #         'name': 'configs',
-                #         'mountPath': '/etc/config',
-                #         'files': {
-                #             'my-charm.conf': open('files/my-charm.conf', 'r').read().decode(),
-                #         },
-                #     },
-                # ],
             },
         ],
     }
-    print("\nSpec:\n")
-    pprint(spec)
+    out = io.StringIO()
+    pprint(spec, out)
+    hookenv.log("Container spec (sans secrets) <<EOM\n{}\nEOM".format(out.getvalue()))
 
-    # Add secrets, now spec logged. TODO: Use real secret management, rather than charm config.
-    secrets = yaml.safe_load(config['container_secrets'])
-    for k, v in secrets.items():
-        container_config[k] = v
+    # Add the secrets after logging
+    container_config.update(interpolate(full_container_config()))
 
     return spec
 
@@ -94,16 +168,8 @@ def resource_image_details(spec):
         'imagePath': docker_image_path,
         'username': docker_image_username,
         'password': docker_image_password,
-    },
+    }
 
 
-# @when_not('db.connected')
-# def db_not_connected():
-#     status.blocked('charm requires a PostgreSQL relation')
-#
-#
-# @when('db.connected')
-# def db_connected():
-#     ep = reactive.endpoint_from_flag('db.connected')
-#     ep.set_database('tasticles')
-#     ep.set_extensions(['citext'])
+# Magic. Add useful flag logging. Remove after next charms.reactive release.
+hookenv.atstart(tracer.install_tracer, tracer.LogTracer())

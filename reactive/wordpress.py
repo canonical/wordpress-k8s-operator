cafdf83fdc5b4cefc50c429cf7f98ecd1dc6725b
Barry Price <barry.price@canonical.com>
2019-12-03 16:12:35 +0700

    Address review comments, fold the osm-common layer methods into our own layer for now to avoid the dependency

--- reactive/wordpress.py
+++ reactive/wordpress.py
@@ -1,9 +1,7 @@
 import io
 import os
-import random
 import re
 import requests
-import string
 from pprint import pprint
 from urllib.parse import urlunparse
 from yaml import safe_load
@@ -11,7 +9,6 @@ from yaml import safe_load
 from charmhelpers.core import host, hookenv
 from charms import reactive
 from charms.layer import caas_base, status
-from charms.osm.k8s import is_pod_up, get_service_ip
 from charms.reactive import hook, when, when_not
 
 
@@ -47,7 +44,8 @@ def deploy_container():
         status.maintenance("configuring container")
         try:
             caas_base.pod_spec_set(spec)
-        except Exception:
+        except Exception as e:
+            hookenv.log("pod_spec_set failed: {}".format(e), hookenv.DEBUG)
             status.blocked("pod_spec_set failed! Check logs and k8s dashboard.")
             return
     else:
@@ -62,7 +60,7 @@ def ready():
 
 
 def sanitized_container_config():
-    """Uninterpolated container config without secrets"""
+    """Container config without secrets"""
     config = hookenv.config()
     if config["container_config"].strip() == "":
         container_config = {}
@@ -78,7 +76,7 @@ def sanitized_container_config():
 
 
 def full_container_config():
-    """Uninterpolated container config with secrets"""
+    """Container config with secrets"""
     config = hookenv.config()
     container_config = sanitized_container_config()
     if container_config is None:
@@ -131,15 +129,15 @@ def first_install():
     if not is_pod_up("website"):
         hookenv.log("Pod not yet ready - retrying")
         return False
+    elif not is_vhost_ready():
+        hookenv.log("Wordpress vhost is not yet listening - retrying")
+        return False
     elif wordpress_configured() or not config["initial_settings"]:
         hookenv.log("No initial_setting provided or wordpress already configured. Skipping first install.")
         return True
-    elif not vhost_ready():
-        hookenv.log("Wordpress vhost is not yet listening - retrying")
-        return False
     hookenv.log("Starting wordpress initial configuration")
     # TODO: more of the below ought to be configurable
-    payload = {"admin_password": mkpasswd(24), "blog_public": "checked", "Submit": "submit"}
+    payload = {"admin_password": host.pwgen(24), "blog_public": "checked", "Submit": "submit"}
     payload.update(safe_load(config["initial_settings"]))
     payload["admin_password2"] = payload["admin_password"]
     if not payload["blog_public"]:
@@ -152,10 +150,6 @@ def first_install():
     call_wordpress("/wp-admin/install.php?step=2", payload=payload)
     host.write_file(os.path.join("/root/", "initial.passwd"), payload["admin_password"], perms=0o400)
     return True
-
-
-def mkpasswd(length=64):
-    return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
 
 def call_wordpress(uri, redirects=True, payload={}):
@@ -180,49 +174,90 @@ def call_wordpress(uri, redirects=True, payload={}):
         return False
 
 
-class wordpress_configured(dict):
+def wordpress_configured():
     """Check whether first install has been completed."""
-
-    def __bool__(self):
-        # Check whether pod is deployed
-        if not is_pod_up("website"):
-            return False
-        # Check if we have WP code deployed at all
-        if not vhost_ready():
-            return False
-        # We have code on disk, check if configured
-        try:
-            r = call_wordpress("/", redirects=False)
-        except requests.exceptions.ConnectionError:
-            return False
-        if r.status_code == 302 and re.match("^.*/wp-admin/install.php", r.headers.get("location", "")):
-            return False
-        else:
-            return True
-
-    def __nonzero__(self):
-        return self.__bool__()
-
-    def is_ready(self):
-        return self.__bool__()
+    # Check whether pod is deployed
+    if not is_pod_up("website"):
+        return False
+    # Check if we have WP code deployed at all
+    if not is_vhost_ready():
+        return False
+    # We have code on disk, check if configured
+    try:
+        r = call_wordpress("/", redirects=False)
+    except requests.exceptions.ConnectionError:
+        return False
+    if r.status_code == 302 and re.match("^.*/wp-admin/install.php", r.headers.get("location", "")):
+        return False
+    else:
+        return True
 
 
-class vhost_ready(dict):
+def is_vhost_ready():
     """Check whether wordpress is available using http."""
+    # Check if we have WP code deployed at all
+    try:
+        r = call_wordpress("/wp-login.php", redirects=False)
+    except requests.exceptions.ConnectionError:
+        return False
+    if r.status_code in (403, 404):
+        return False
+    else:
+        return True
 
-    def __bool__(self):
-        # Check if we have WP code deployed at all
-        try:
-            r = call_wordpress("/wp-login.php", redirects=False)
-        except requests.exceptions.ConnectionError:
-            return False
-        if r.status_code in (403, 404):
-            return False
+
+def get_service_ip(endpoint):
+    try:
+        info = hookenv.network_get(endpoint, hookenv.relation_id())
+        if 'ingress-addresses' in info:
+            addr = info['ingress-addresses'][0]
+            if len(addr):
+                return addr
         else:
-            return True
+            hookenv.log("No ingress-addresses: {}".format(info))
+    except Exception as e:
+        hookenv.log("Caught exception checking for service IP: {}".format(e))
 
-    def __nonzero__(self):
-        return self.__bool__()
+    return None
 
-    def is_ready(self):
-        return self.__bool__()
+
+def is_pod_up(endpoint):
+    """Check to see if the pod of a relation is up.
+
+    application-vimdb: 19:29:10 INFO unit.vimdb/0.juju-log network info
+
+    In the example below:
+    - 10.1.1.105 is the address of the application pod.
+    - 10.152.183.199 is the service cluster ip
+
+    {
+        'bind-addresses': [{
+            'macaddress': '',
+            'interfacename': '',
+            'addresses': [{
+                'hostname': '',
+                'address': '10.1.1.105',
+                'cidr': ''
+            }]
+        }],
+        'egress-subnets': [
+            '10.152.183.199/32'
+        ],
+        'ingress-addresses': [
+            '10.152.183.199',
+            '10.1.1.105'
+        ]
+    }
+    """
+    try:
+        info = hookenv.network_get(endpoint, hookenv.relation_id())
+
+        # Check to see if the pod has been assigned it's internal and
+        # external ips
+        for ingress in info['ingress-addresses']:
+            if len(ingress) == 0:
+                return False
+    except Exception:
+        return False
+
+    return True

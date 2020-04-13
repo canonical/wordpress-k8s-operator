b5ee19641580f2510fa835525f24c0972b33e493
Thomas Cuthbert <thomas.cuthbert@canonical.com>
2020-04-13 10:08:05 +0800

    Updated missing charm config block status message to match log statement

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -35,12 +35,13 @@ class TestWordpressK8sCharm(unittest.TestCase):
         self.assertTrue(want_true)
 
         # Test for invalid model config.
+        want_msg_fmt = "Missing required config: {}"
         want_keys = ("image", "db_host", "db_name", "db_user", "db_password")
         for wanted_key in want_keys:
             self.harness.charm.model.config[wanted_key] = ""
             want_false = self.harness.charm.is_valid_config()
             self.assertFalse(want_false)
-            self.assertLogs("Missing required config: {}".format(wanted_key), level="INFO")
+            self.assertLogs(want_msg_fmt.format(wanted_key), level="INFO")
             self.harness.charm.model.config = copy.deepcopy(self.test_model_config)
 
         # Test for missing initial_settings in model config.
@@ -53,8 +54,8 @@ class TestWordpressK8sCharm(unittest.TestCase):
         # Test unit status msg.
         for wanted_key in want_keys:
             self.harness.charm.model.config[wanted_key] = ""
-        expected_msg = "image db_host db_name db_user db_password config is required"
+        expected_msg = want_msg_fmt.format(" ".join(want_keys))
         self.harness.charm.is_valid_config()
         self.assertIsInstance(self.harness.charm.unit.status, BlockedStatus)
         self.assertEqual(self.harness.charm.unit.status.message, expected_msg)
-        self.assertLogs("Missing required config: image db_host db_name db_user db_password", level="INFO")
+        self.assertLogs(expected_msg, level="INFO")

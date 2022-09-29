0ce5fe36c907c09755f40841a9cba041d9010aa9
Weii Wang <weii.wang@canonical.com>
2022-09-29 23:43:00 +0800

    Add some comments on integration test cli options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,19 @@
 import pytest
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: pytest.Parser):
+    # --openstack-rc points to an openstack credential file in the "rc" file style
+    # Here's an example of that file
+    # $ echo ~/openrc
+    # export OS_REGION_NAME=RegionOne
+    # export OS_PROJECT_DOMAIN_ID=default
+    # export OS_AUTH_URL=http://10.0.0.1/identity
+    # export OS_TENANT_NAME=demo
+    # export OS_USER_DOMAIN_ID=default
+    # export OS_USERNAME=demo
+    # export OS_VOLUME_API_VERSION=3
+    # export OS_AUTH_TYPE=password
+    # export OS_PROJECT_NAME=demo
+    # export OS_PASSWORD=nomoresecret
+    # export OS_IDENTITY_API_VERSION=3
     parser.addoption("--openstack-rc", action="store", default="")

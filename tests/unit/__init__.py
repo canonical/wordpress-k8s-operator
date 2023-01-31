7cdf5a457dc272137d333d2af7f312b0704d6049
Niels Robin-Aubertin <git@niels.fr>
2023-01-31 07:51:29 +0100

    Add type ignore, update copyright date

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,8 +1,9 @@
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Enable ops.testing.SIMULATE_CAN_CONNECT for all unit tests."""
 
 import ops.testing
 
-ops.testing.SIMULATE_CAN_CONNECT = True
+# The attribute exists
+ops.testing.SIMULATE_CAN_CONNECT = True  # type: ignore[attr-defined]

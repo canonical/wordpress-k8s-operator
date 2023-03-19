d2c6ac9b31c889411b8302c6f49e4747b198b3e6
Weii Wang <weii.wang@canonical.com>
2023-03-19 14:31:08 +0800

    Update test_new_upgrade.py

--- tests/integration/test_new_upgrade.py
+++ tests/integration/test_new_upgrade.py
@@ -178,7 +178,7 @@ async def build_and_upgrade_fixture(
 
 
 @pytest.mark.usefixtures("build_and_upgrade")
-async def test_wordpress_upgrade(get_unit_ip_list, screenshot_dir):
+async def test_wordpress_upgrade(model, get_unit_ip_list, screenshot_dir):
     """
     arrange: the WordPress charm has been upgraded.
     act: browser the WordPress website powered by the new charm.
@@ -212,7 +212,7 @@ async def test_wordpress_upgrade(get_unit_ip_list, screenshot_dir):
                     f"access image {url} should return a valid image file"
                 ) from exc
 
-    await ops_test.model.wait_for_idle(status=ops.model.ActiveStatus.name)  # type: ignore
+    await model.wait_for_idle(status=model.ActiveStatus.name)  # type: ignore
     unit_ip_list = await get_unit_ip_list()
     for idx, unit_ip in enumerate(unit_ip_list):
         await screenshot(f"http://{unit_ip}", screenshot_dir / f"wordpress-after-{idx}.png")

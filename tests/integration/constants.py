c9c29dc7f3856b7d7cfffa4000344366b29cd67b
charlie4284 <yangsoo.yoon@canonical.com>
2023-03-29 15:21:29 +0800

    fix: mypy type ignore

--- tests/integration/constants.py
+++ tests/integration/constants.py
@@ -7,5 +7,6 @@ import typing
 
 from ops.model import ActiveStatus, BlockedStatus
 
-ACTIVE_STATUS_NAME = typing.cast(str, ActiveStatus.name)
-BLOCKED_STATUS_NAME = typing.cast(str, BlockedStatus.name)
+# mypy has trouble to inferred types for variables that are initialized in subclasses.
+ACTIVE_STATUS_NAME = typing.cast(str, ActiveStatus.name)  # type: ignore
+BLOCKED_STATUS_NAME = typing.cast(str, BlockedStatus.name)  # type: ignore

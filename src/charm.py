ca0224740e9658b6ff171d088ce9d9c89f43de3a
Franco Forneron <franco.forneron@canonical.com>
2023-09-12 10:18:35 -0300

    rollback saml plugin

--- src/charm.py
+++ src/charm.py
@@ -103,7 +103,6 @@ class WordpressCharm(CharmBase):
         "genesis-columns-advanced",
         "line-break-shortcode",
         "wp-mastodon-share",
-        "miniorange-saml-20-single-sign-on",
         "no-category-base-wpml",
         "openid",
         "wordpress-launchpad-integration",

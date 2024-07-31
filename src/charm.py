6e3ba788f35accf34e95937d38760fe9c4bf1095
ali ugur <ali.ugur@canonical.com>
2024-07-31 18:11:16 +0300

    Added the `update-database` action. I need feedback and advice on what kind of tests I need to write.

--- src/charm.py
+++ src/charm.py
@@ -182,6 +182,7 @@ class WordpressCharm(CharmBase):
         self.framework.observe(
             self.on.rotate_wordpress_secrets_action, self._on_rotate_wordpress_secrets_action
         )
+        self.framework.observe(self.on.update_database_action, self._on_update_database_action)
 
         self.framework.observe(self.on.leader_elected, self._setup_replica_data)
         self.framework.observe(self.on.uploads_storage_attached, self._reconciliation)
@@ -265,6 +266,23 @@ class WordpressCharm(CharmBase):
         # Leader need to call `_reconciliation` manually.
         # Followers call it automatically due to relation_changed event.
         self._reconciliation(event)
+        event.set_results({"result": "ok"})
+
+    def _on_update_database_action(self, event: ActionEvent):
+        """Handle the update-database action.
+
+        This action is to upgrade the database schema after the WordPress version is upgraded.
+
+        Args:
+            event: Used for returning result or failure of action.
+        """
+        logger.info("update-database is running: bip bop bap...")
+        cmd = ["wp", "core", "update-db"]
+
+        result = self._wrapped_run_wp_cli(cmd, timeout=600)
+        if not result.success:
+            event.fail(result.message)
+
         event.set_results({"result": "ok"})
 
     @staticmethod

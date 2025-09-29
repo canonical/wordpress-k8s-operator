124211f90b6cacc848eef009d256325d53e35c44
erinecon <erin.conley@canonical.com>
2025-09-29 07:55:22 -0400

    update cookie banner for google analytics tracking

--- docs/conf.py
+++ docs/conf.py
@@ -207,8 +207,8 @@ sitemap_excludes = [
 # Template and asset locations
 #######################
 
-#html_static_path = ["_static"]
-#templates_path = ["_templates"]
+html_static_path = [".sphinx/_static"]
+templates_path = ["_templates"]
 
 
 #############
@@ -300,12 +300,16 @@ exclude_patterns = [
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
+html_css_files = [
+    'cookie-banner.css'
+]
 
 
 # Adds custom JavaScript files, located under 'html_static_path'
 
-# html_js_files = []
+html_js_files = [
+    'js/bundle.js'
+]
 
 
 # Specifies a reST snippet to be appended to each .rst file

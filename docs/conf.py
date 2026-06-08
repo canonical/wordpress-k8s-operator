9eddb2587061c00be61f3d9d6da20c5e054d3dea
erinecon <erin.conley@canonical.com>
2026-06-08 11:18:16 -0400

    chore: add input YAML for copilot skill

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import yaml
+version = f"{os.environ.get('READTHEDOCS_VERSION', 'local')}"
 
 # Configuration for the Sphinx documentation builder.
 # All configuration specific to your project should be done in this file.
@@ -70,7 +71,7 @@ copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://documentation.ubuntu.com/wordpress-k8s-charm/"
+ogp_site_url = f"https://canonical.com/juju/docs/wordpress-k8s-charm/{version}/"
 
 
 # Preview name of the documentation website
@@ -174,7 +175,7 @@ html_theme_options = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-slug = 'wordpress-k8s-charm'
+slug = 'juju/docs/wordpress-k8s-charm'
 
 #######################
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
@@ -182,7 +183,7 @@ slug = 'wordpress-k8s-charm'
 
 # Use RTD canonical URL to ensure duplicate pages have a specific canonical URL
 
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_baseurl = f"https://canonical.com/juju/docs/wordpress-k8s-charm/{version}/"
 
 # sphinx-sitemap uses html_baseurl to generate the full URL for each page:
 
@@ -307,7 +308,8 @@ html_css_files = [
 # Adds custom JavaScript files, located under 'html_static_path'
 
 html_js_files = [
-    'js/bundle.js'
+    'js/bundle.js',
+    'js/overwrite_links.js',
 ]
 
 
